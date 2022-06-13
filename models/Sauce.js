25d71e5a68295d1af3b40dbf84d5fa998a563bc5
Manon Garnier <manon.garnier.lehy@gmail.com>
2022-06-13 16:37:43 +0200

    ajout commentaire

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// On crée un schéma de données avec les champs requis pour une sauce
 const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true },
     name: { type: String, required: true },
